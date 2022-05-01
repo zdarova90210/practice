9506af61a0a639774cf9770ab187600088f5456d
Maksim Mirbabaev <zdarova90210@gmail.com>
2022-05-01 23:28:22 +0300

    Add reduce polyfill

--- polyfills/main.js
+++ polyfills/main.js
@@ -17,3 +17,27 @@ if (!Array.prototype.myMap) {
     return result;
   }
 }
+
+if (!Array.prototype.myReduce) {
+  Array.prototype.myReduce = function (callback, initValue) {
+    if (!(this instanceof Array || this instanceof String)) {
+      throw new TypeError(`Array.prototype.myReduce was called on wrong type.`);
+    }
+    if (typeof callback !== 'function') {
+      throw new TypeError(`Array.prototype.myReduce ${callback} is not a function.`);
+    }
+
+    let acc = arguments.length >= 2 ? initValue : this[0];
+    let startFrom = arguments.length >= 2 ? 0 : 1;
+    for (let i = startFrom; i < this.length; i++) {
+      acc = callback(acc, this[i], i, this);
+    }
+    return acc;
+  }
+}
+
+// const testArr = [1,2,3,4,5];
+// const myReduceResult = testArr.myReduce((acc, curr, i, arr) => {
+//   return acc + curr;
+// }, 10);
+
