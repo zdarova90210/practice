6f31e428e826137b6865834f4359b8147bbee769
Maksim Mirbabaev <zdarova90210@gmail.com>
2022-05-23 21:54:51 +0300

    Add "Guess my number" markup.

--- 00-sandbox/main.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const fireBtn = document.querySelector('#fireBtn');
-
-fireBtn.addEventListener('click', myFunction, false);
-
-function myFunction($event) {
-  console.log($event.target.id);
-}
