8f53fae27bbfb7281a9685e23aa156b1400dbc2a
Maksim Mirbabaev <zdarova90210@gmail.com>
2022-06-02 21:51:37 +0300

    implement simple modal window.

--- modal-window/script.js
+++ modal-window/script.js
@@ -1,1 +1,28 @@
 'use strict';
+
+
+const showModalBtns = document.querySelectorAll('.show-modal');
+const closeModalEl = document.querySelector('.close-modal');
+const overlayEl = document.querySelector('.overlay');
+const modalEl = document.querySelector('.modal');
+const closeEls = document.querySelectorAll('.close-modal, .overlay');
+
+document.body.addEventListener('keydown', doKeyboardAction);
+for (let i = 0; i < showModalBtns.length; i++) showModalBtns[i].addEventListener('click', showModal);
+for (let i = 0; i < closeEls.length; i++) closeEls[i].addEventListener('click', closeModal);
+
+function showModal() {
+  overlayEl.classList.remove('hidden');
+  modalEl.classList.remove('hidden');
+}
+
+function closeModal() {
+  console.log('close');
+  overlayEl.classList.add('hidden');
+  modalEl.classList.add('hidden');
+}
+
+function doKeyboardAction($event) {
+  if ($event.key === 'Escape') closeModal();
+}
+
