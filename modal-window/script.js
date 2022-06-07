04823a0d086a7689a2f9b4c59204740d4db63d7c
Maksim Mirbabaev <zdarova90210@gmail.com>
2022-06-07 15:22:47 +0300

    done with modals and start to do implement pig game.

--- modal-window/script.js
+++ modal-window/script.js
@@ -1,15 +1,11 @@
 'use strict';
 
-
 const showModalBtns = document.querySelectorAll('.show-modal');
-const closeModalEl = document.querySelector('.close-modal');
 const overlayEl = document.querySelector('.overlay');
 const modalEl = document.querySelector('.modal');
 const closeEls = document.querySelectorAll('.close-modal, .overlay');
 
-document.body.addEventListener('keydown', doKeyboardAction);
-for (let i = 0; i < showModalBtns.length; i++) showModalBtns[i].addEventListener('click', showModal);
-for (let i = 0; i < closeEls.length; i++) closeEls[i].addEventListener('click', closeModal);
+init();
 
 function showModal() {
   overlayEl.classList.remove('hidden');
@@ -17,7 +13,6 @@ function showModal() {
 }
 
 function closeModal() {
-  console.log('close');
   overlayEl.classList.add('hidden');
   modalEl.classList.add('hidden');
 }
@@ -26,3 +21,8 @@ function doKeyboardAction($event) {
   if ($event.key === 'Escape') closeModal();
 }
 
+function init() {
+  document.body.addEventListener('keydown', doKeyboardAction);
+  for (let i = 0; i < showModalBtns.length; i++) showModalBtns[i].addEventListener('click', showModal);
+  for (let i = 0; i < closeEls.length; i++) closeEls[i].addEventListener('click', closeModal);
+}
